dcb210259d83ee3b64bc1083d849872417ff3ef8
like2 <like2@staff.weibo.com>
2017-12-18 16:24:11 +0800

    first commit

---
+++ app/src/main/java/cn/ezandroid/goboard/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.ezandroid.goboard.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(cn.ezandroid.goboard.R.layout.activity_main);
+    }
+}
